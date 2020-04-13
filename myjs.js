66ba5ca3ba68a224019364aea9802bdeb14ec701
colin <owens106@purdue.edu>
2020-04-12 23:32:05 -0400

    troll commit

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
 function changeButton(){
     var button = document.getElementById("button1");
     console.log(button);
-    button.textContent="I LOVE YOU BB";
+    button.textContent="clare sucks hehehhehe";
     return;
 }
