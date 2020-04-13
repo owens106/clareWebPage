e1f9c357af4950981c1a2bf2eaf4b15b913e3d26
colin <owens106@purdue.edu>
2020-04-12 23:33:45 -0400

    revert commit

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
 function changeButton(){
     var button = document.getElementById("button1");
     console.log(button);
-    button.textContent="clare sucks hehehhehe";
+    button.textContent="I LOVE YOU BB";
     return;
 }
