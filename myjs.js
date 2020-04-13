2a6e76621eec0db88e1d27e0c16e8fd4b82d1408
colin <owens106@purdue.edu>
2020-04-13 12:25:43 -0400

    added password

--- myjs.js
+++ myjs.js
@@ -1,6 +1,22 @@
 function changeButton(){
     var button = document.getElementById("button1");
-    console.log(button);
     button.textContent="I LOVE YOU BB";
     return;
+}
+
+function pwcheck(){
+  console.log("made it to pw check")
+  var div = document.getElementById("maindiv")
+  var formVal = document.getElementById("pw").value
+  console.log(formVal)
+  console.log(div)
+  var pw = "ilovebb24/7"
+  if(pw === formVal){
+     div.style.visibility = "visible"
+     console.log(div)
+
+  }
+  else{
+      
+  }
 }
